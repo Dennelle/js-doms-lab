2c14bf78b63eb4ba33c7277ebaae67d8f29499b7
Dennelle <dennelle.walton@gmail.com>
2023-09-29 05:21:25 -0400

    completed all of task 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Task 1.0 Select and cache the <main> element in a variable named mainEl.
 const mainEl = document.querySelector("main")
-console.log(mainEl)
+//console.log(mainEl)
 
 // Task 1.1
 // Set the background color of mainEl using the --main-bg CSS custom property.
@@ -19,6 +19,23 @@ console.log(h1)
 // Add a class of flex-ctr to mainEl.
 // Hint: Element.classList API
 
-mainEl.classList.add("flex-ctr")
-console.log(mainEl)
+mainEl.classList.add('flex-ctr')
+//console.log(mainEl)
 mainEl.style.textAlign = 'center'
+
+// Task 2.0
+// Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+const topMenuEl = document.getElementById('top-menu')
+//console.log(topMenuEl);
+
+// Task 2.1
+// Set the height topMenuEl element to be 100%.
+topMenuEl.style.height = "100%"
+
+// Task 2.2
+// Set the background color of topMenuEl using the --top-menu-bg CSS custom property.
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
+
+// Task 2.3
+// Add a class of flex-around to topMenuEl.
+topMenuEl.classList.add('flex-round')
